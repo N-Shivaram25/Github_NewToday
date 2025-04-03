8d017e95faf0d4fe53a2d412486c04eaa993f352
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-03 23:04:44 +0530

    changed c ,cpp, java

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,17 +1,7 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-void sortedSquares(int* nums, int size, int* result) {
-    int left = 0, right = size - 1, pos = size - 1;
-
-    while (left <= right) {//fgdg
-        if (abs(nums[left]) > abs(nums[right])) {
-            result[pos] = nums[left] * nums[left];
-            left++;
-        } else {
-            result[pos] = nums[right] * nums[right];
-            right--;
-        }//dsfds
-        pos--;//1//1
-    }
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    // Implement your solution here
 }
--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,22 +1,10 @@
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-vector<int> sortedSquares(vector<int>& nums) {
-    int left = 0, right = nums.size() - 1, pos = nums.size() - 1;
-    vector<int> result(nums.size());
-//sdfsdf
-    while (left <= right) {
-        if (abs(nums[left]) > abs(nums[right])) {
-            result[pos] = nums[left] * nums[left];
-            left++;//ss
-        } else {
-            result[pos] = nums[right] * nums[right];
-            right--;
-        }
-        pos--;//2dsfsdss
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        // Implement your solution here
     }
-
-    return result;//vdsvd
-}
+};
