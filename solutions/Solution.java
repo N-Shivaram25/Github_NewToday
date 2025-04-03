e13b33d00bb7cf9dbdbf7148d0643c2b5befe0ac
Shiva Ram <naradashishivaram25@gmail.com>
2025-04-03 23:46:26 +0530

    changed c ,cpp, java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int[] sortedSquares(int[] nums) {
-        // Implement your solution heressss
+        // Implement your solution heresssss
     }
 }
--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -3,6 +3,6 @@
 class Solution {
 public:
     std::vector<int> sortedSquares(std::vector<int>& nums) {
-        return {}; // Returning an empty vector to ensure test case failures
+        return {}; // Returnsing an empty vector to ensure test case failures
     }
 };
